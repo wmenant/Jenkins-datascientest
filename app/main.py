62b8ef8560914574d0fb6b2e1b22299281c9fa08
William Menant Datascientest <wmenant@live.com>
2024-03-28 16:43:15 +0000

    Failed version

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest, and we did it again and again! We build a new complete release CI/CD Pipeline !!"}
+    return {"Hello": "We Love Datascientest, and we did it again and again! We build a new complete release CI/CD Pipeline !!"  + 2}
